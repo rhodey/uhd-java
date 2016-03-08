dd9c557dfe93ef2d9a6308878b1289333ad98cc7
Rhodey Orbits <rhodey@anhonesteffort.org>
2016-03-08 12:22:19 -0600

    implement range_t

---
+++ src/main/java/org/anhonesteffort/uhd/types/Range.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2016 An Honest Effort LLC.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.anhonesteffort.uhd.types;
+
+import org.bytedeco.javacpp.Loader;
+import org.bytedeco.javacpp.Pointer;
+import org.bytedeco.javacpp.annotation.Name;
+import org.bytedeco.javacpp.annotation.Platform;
+
+@Platform(
+    link    = "uhd",
+    include = {"<uhd/types/ranges.hpp>"}
+)
+@Name("uhd::range_t")
+public class Range extends Pointer {
+
+  static { Loader.load(); }
+
+  public Range(double value) { allocate(value); }
+  public Range(double start, double stop, double step) { allocate(start, stop, step); }
+
+  private native void allocate(double value);
+  private native void allocate(double start, double stop, double step);
+
+  public native double start();
+  public native double stop();
+  public native double step();
+
+}
